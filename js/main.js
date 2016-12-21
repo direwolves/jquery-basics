390c47cd94900fb9f07e6d8b1bca4f5ca3485612
Sean Xavier <seanxhallion@gmail.com>
2016-12-20 19:01:20 -0500

    jquery document on ready

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+// write a function that takes a first name as an argument
+// Have it log your firstname to the console
+
+var myFirstName = function(firstName){
+	console.log('My first name is ' + firstName);
+
+}
+// invoke that function.
+myFirstName('Sean');
+
+$(document).on('ready', function(){
+	// now I've ensured my DOM is ready, I may proceed
+
+
+
+}) // closes doc.ready
