6abf2868d42762c5a2fefa537c50dbe85989157a
Sean Xavier <seanxhallion@gmail.com>
2016-12-20 19:14:37 -0500

    Callbacks

--- js/main.js
+++ js/main.js
@@ -13,6 +13,21 @@ myFirstName('Sean');
 $(document).on('ready', function(){
 	// now I've ensured my DOM is ready, I may proceed
 
+// CALLBACKS!!!
+	// create an anon function that we will use as a callback
+	var bake = function(){
+		console.log('You may now bake!');
+	}
+
+	// create an anon function that accepts an argument
+	// invole that argument
+	var preHeatOven = function(callback){
+		console.log('oven is pre-heated');
+		callback()// invoke the argument as a function
+	}
+
+	// now invoke preHeatOven, passing in bake as an argument
+	preHeatOven(bake);
 
 
 }) // closes doc.ready
